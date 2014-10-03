838dfcc910727f0b007e2f547449054719218ddd
johnheroy <johnheroy@gmail.com>
2014-10-03 16:11:03 -0700

    add boilerplate to db functions

--- SQL/db.js
+++ SQL/db.js
@@ -1,5 +1,4 @@
 var mysql = require('mysql');
-var mysql = require('mysql');
 /* If the node mysql module is not found on your system, you may
  * need to do an "sudo npm install -g mysql". */
 
@@ -7,12 +6,11 @@ var mysql = require('mysql');
  * database: "chat" specifies that we're using the database called
  * "chat", which we created by running schema.sql.*/
 var dbConnection = mysql.createConnection({
-  user: "",
+  user: "root",
   password: "",
   database: "chat"
 });
 
-dbConnection.connect();
 /* Now you can make queries to the Mysql database using the
  * dbConnection.query() method.
  * See https://github.com/felixge/node-mysql for more details about
@@ -22,13 +20,48 @@ dbConnection.connect();
 
 
 exports.findAllMessages = function(cb){
+  dbConnection.connect();
+  var queryString = 'SELECT * FROM ?';
+  var queryArgs = ['MESSAGES'];
+  dbConnection.query(queryString, queryArgs, function(err, results){
+    if (err) {throw err;}
+
+    cb(results);
+  });
+
+  dbConnection.end();
 };
 
 exports.findUser = function(username, cb){
+  dbConnection.connect();
+  var queryString = '';
+  var queryArgs = [];
+  dbConnection.query(queryString, queryArgs, function(err, results){
+    if (err) {throw err;}
+
+  });
+  dbConnection.end();
 };
 
 exports.saveUser = function(username, cb){
+  dbConnection.connect();
+  var queryString = '';
+  var queryArgs = [];
+  dbConnection.query(queryString, queryArgs, function(err, results){
+    if (err) {throw err;}
+
+  });
+  dbConnection.end();
 };
 
 exports.saveMessage = function(message, userid, roomname, cb){
+  dbConnection.connect();
+  var queryString = '';
+  var queryArgs = [];
+  dbConnection.query(queryString, queryArgs, function(err, results){
+    if (err) {throw err;}
+
+  });
+  dbConnection.end();
 };
+
