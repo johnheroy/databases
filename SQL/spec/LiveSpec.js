e34779d15a168517e5d17d7a138145225863c4dc
johnheroy <johnheroy@gmail.com>
2014-10-03 15:58:41 -0700

    Start specs

--- SQL/spec/LiveSpec.js
+++ SQL/spec/LiveSpec.js
@@ -38,15 +38,10 @@ describe("Persistent Node Chat Server", function() {
                     roomname: "Hello"}
             },
             function () {
-              /* Now if we look in the database, we should find the
-               * posted message there. */
 
-              var queryString = "";
-              var queryArgs = [];
-              /* TODO: Change the above queryString & queryArgs to match your schema design
-               * The exact query string and query args to use
-               * here depend on the schema you design, so I'll leave
-               * them up to you. */
+              var queryString = "SELECT * FROM ?"; // SELECT ...
+              var queryArgs = ['messages']; // messages
+
               dbConnection.query( queryString, queryArgs,
                 function(err, results) {
                   // Should have one result:
@@ -61,7 +56,7 @@ describe("Persistent Node Chat Server", function() {
             });
   });
 
-  it("Should output all messages from the DB", function(done) {
+  xit("Should output all messages from the DB", function(done) {
     // Let's insert a message into the db
     var queryString = "";
     var queryArgs = [];
