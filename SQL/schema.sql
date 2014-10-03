b1843cb57dd907b4427abe4b6621d9eb8e163440
johnheroy <johnheroy@gmail.com>
2014-10-03 15:28:00 -0700

    Write schema and clone client and server

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,20 +1,80 @@
-CREATE DATABASE chat;
+
 
 USE chat;
+-- ---
+-- Globals
+-- ---
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'Messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `Messages`;
+
+CREATE TABLE `Messages` (
+  PRIMARY KEY (`id`),
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `User_id` INTEGER NULL DEFAULT NULL,
+  `Text` VARCHAR(140) NULL DEFAULT NULL,
+  `Created_at` TIMESTAMP DEFAULT NOW(),
+  `Room_id` INTEGER(8) NULL DEFAULT NULL
 );
 
+-- ---
+-- Table 'Users'
+--
+-- ---
+
+DROP TABLE IF EXISTS `Users`;
+
+CREATE TABLE `Users` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `Name` VARCHAR(30) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'Rooms'
+--
+-- ---
+
+DROP TABLE IF EXISTS `Rooms`;
+
+CREATE TABLE `Rooms` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `Name` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `Messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `Rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `Messages` (`id`,`User id`,`Text`,`Created at`,`Room id`) VALUES
+-- ('','','','','');
+-- INSERT INTO `Users` (`id`,`Name`) VALUES
+-- ('','');
+-- INSERT INTO `Rooms` (`id`,`Name`) VALUES
+-- ('','');
+
 /* Create other tables and define schemas for them here! */
-
-
-
 
 /*  Execute this file from the command line by typing:
  *    mysql < schema.sql
  *  to create the database and the tables.*/
-
-
-
-
