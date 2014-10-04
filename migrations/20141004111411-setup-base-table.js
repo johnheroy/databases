fc6a6ea27a14dc09aebf0de441c34e33d1dc8dd9
johnheroy <johnheroy@gmail.com>
2014-10-04 11:45:50 -0700

    Setup table using migration

--- migrations/20141004111411-setup-base-table.js
+++ migrations/20141004111411-setup-base-table.js
@@ -3,6 +3,43 @@
 module.exports = {
   up: function(migration, DataTypes, done) {
     // add altering commands here, calling 'done' when finished
+    migration.createTable(
+      'Messages',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        User_id: DataTypes.INTEGER,
+        Text: DataTypes.STRING,
+        Room_id: DataTypes.INTEGER
+      }
+    );
+
+    migration.createTable(
+      'Users',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        Name: DataTypes.STRING
+      }
+    );
+
+    migration.createTable(
+      'Rooms',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        Name: DataTypes.STRING
+      }
+    );
     done();
   },
 
